e3e217e9616ebf09aef0bdff78ee63c2a52a9b0e
Boniface Pereira <bonifacepereira@gmail.com>
2017-03-11 11:35:26 +0530

    adsblocker notification can be dismissed forever.

--- demo/demo.js
+++ demo/demo.js
@@ -13,27 +13,50 @@ $("span.version").html(version || "");
 $(document).ready(function () {
     prettyPrint();
     setTimeout(function () {
-        return;
-        $.confirm({
-            title: 'Ahh, ad blockers',
-            icon: 'fa fa-meh-o',
-            content: "Ads aren't what you're here for. But ads help me support my work. So, please consider to add this domain to your blocker's whitelist. <br>I'd really appreciate it.",
-            closeIcon: false,
-            buttons: {
-                dontCare: {
-                    text: 'I dont care &gt;:(',
+        if (typeof google_jobrunner == 'undefined' && !localStorage['adsok']) {
+            $.confirm({
+                title: 'Hmmm, ad blocker',
+                theme: 'material',
+                content: "Ads aren't what you're here for. But ads help me support my work. So, please consider to add this domain to your blocker's whitelist.",
+                closeIcon: false,
+                buttons: {
+                    done: {
+                        text: 'Did it',
+                        btnClass: 'btn-green',
+                        action: function () {
+                            $.alert('I really appreciate this. Thank you.', 'You\'re awesome');
+                            setTimeout(function () {
+                                location.reload();
+                            }, 1000);
+                        }
+                    },
+                    doNotAskAgain: {
+                        text: 'Don\'t ask',
+                        action: function () {
+                            localStorage['adsok'] = true;
+                        }
+                    },
+                    donate: {
+                        text: 'Donate',
+                        btnClass: 'btn-default',
+                        action: function () {
+                            window.open('https://www.paypal.me/bonifacepereira', '_blank');
+                            localStorage['adsok'] = true;
+                        }
+                    }
+
                 }
-            }
-        });
+            });
+        }
     }, 1000);
 
-    $('.change-format-css').click(function(){
+    $('.change-format-css').click(function () {
         $('.change-format-css').addClass('active');
         $('.change-format-less').removeClass('active');
         $('.format-less').hide();
         $('.format-css').show();
     });
-    $('.change-format-less').click(function(){
+    $('.change-format-less').click(function () {
         $('.change-format-less').addClass('active');
         $('.change-format-css').removeClass('active');
         $('.format-css').hide();
